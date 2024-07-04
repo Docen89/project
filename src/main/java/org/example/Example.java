f059c9b47992df1d7cb4be819f1ce899da218a4b
unknown <docen89@ya.ru>
2024-07-04 15:52:14 +0500

    add example1_2

---
+++ src/main/java/org/example/Example.java
@@ -0,0 +1,10 @@
+/*
+Простая программа на java.
+Назовите этот файл Example.java
+ */
+class Example {
+    //программа начинается с вызова main ().
+    public static void main (String[] agrs) {
+        System.out.println("Простая программа на языке Java.");
+    }
+}
